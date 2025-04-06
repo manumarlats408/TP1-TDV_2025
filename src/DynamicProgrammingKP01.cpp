2844550ff469206f4f7caf43e93675b125a7d79c
Manuel Marlats <85189147+manumarlats408@users.noreply.github.com>
2025-04-06 17:33:45 -0300

    Add files via upload

--- src/DynamicProgrammingKP01.cpp
+++ src/DynamicProgrammingKP01.cpp
@@ -6,8 +6,9 @@
 DynamicProgrammingKP01::DynamicProgrammingKP01() {}
 
 int max(int a, int b) {
-    return (a > b) ? a : b;
+    return (a > b) ? a : b; //O(1)
 }
+//O(1)
 
 
 Solution DynamicProgrammingKP01::solve(const KP01withCGInstance& instance) {
@@ -18,37 +19,43 @@ Solution DynamicProgrammingKP01::solve(const KP01withCGInstance& instance) {
 
     std::vector<std::vector<int>> m(n+1,std::vector<int>(C+1)); //O(n*C)
 
-    for(int i=0;i<=n;i++){
-        m[i][0]=0;
+    for(int i=0;i<=n;i++){ //O(n)
+        m[i][0]=0; //O(1)
     }
-    for(int c=0;c<=C;c++){
-        m[0][c]=0;
+    for(int c=0;c<=C;c++){ //O(C)
+        m[0][c]=0; //O(1)
     }
 
-    for(int k=1;k<=n;k++){
-        for(int c=1;c<=C;c++){
-            if(instance.getWeight(k-1)>c){
-                m[k][c]=m[k-1][c];
+    for(int k=1;k<=n;k++){ //O(n)
+        for(int c=1;c<=C;c++){ //O(C)
+            if(instance.getWeight(k-1)>c){ //O(1)
+                m[k][c]=m[k-1][c]; //O(1)
             }else{
-                m[k][c]=max(m[k-1][c],instance.getProfit(k-1)+m[k-1][c-instance.getWeight(k-1)]);
-                if(m[k][c]!=m[k-1][c]){
+                m[k][c]=max(m[k-1][c],instance.getProfit(k-1)+m[k-1][c-instance.getWeight(k-1)]); //O(1)
+                if(m[k][c]!=m[k-1][c]){ //O(1)
                 }
             }
         }
     }
 
-    int c = C;
-    for (int k = n; k >= 1; --k) {
-        if (m[k][c] != m[k - 1][c]) {
-            int idx = k - 1;  // índice real del ítem
-            b.addItem(idx);
-            b.sumar_peso(instance.getWeight(idx));
-            b.sumar_beneficio(instance.getProfit(idx));
-            c -= instance.getWeight(idx);
+    int c = C; //O(1)
+    for (int k = n; k >= 1; --k) { //O(n)
+        if (m[k][c] != m[k - 1][c]) { //O(1)
+            int l = k - 1;  //O(1)
+            b.addItem(l); //O(1)
+            b.sumar_peso(instance.getWeight(l)); //O(1)
+            b.sumar_beneficio(instance.getProfit(l)); //O(1)
+            c -= instance.getWeight(l); //O(1)
         }
-        // Si no se incluye, no hacemos nada
+        
     }
     
 
-    return b;
+    return b; //O(1)
 }
+
+// O(1) + O(1) + O(n) + O(n*C) + O(n) + O(C) + O(n*C) + O(1) + O(n)
+// O(1 + n + n*C + C)
+//O(n*C)
+// n siendo Número de ítems
+// c siendo Capacidad total de la mochila
